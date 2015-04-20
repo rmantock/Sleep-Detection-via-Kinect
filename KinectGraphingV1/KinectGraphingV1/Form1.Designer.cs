2ac40bce777e69063a910f101006fa4fd5cfba2d
Rob Mantock <rmantock94@gmail.com>
2015-04-20 19:24:17 -0400

    init repo

---
+++ KinectGraphingV1/KinectGraphingV1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace KinectGraphingV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chtKinectActivity = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chtKinectActivity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chtKinectActivity
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chtKinectActivity.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chtKinectActivity.Legends.Add(legend1);
+            this.chtKinectActivity.Location = new System.Drawing.Point(13, 13);
+            this.chtKinectActivity.Name = "chtKinectActivity";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Sleep Activity";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chtKinectActivity.Series.Add(series1);
+            this.chtKinectActivity.Size = new System.Drawing.Size(633, 340);
+            this.chtKinectActivity.TabIndex = 0;
+            this.chtKinectActivity.Text = "Sleep Activity";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(571, 359);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "&Run Kinect";
+            this.btnRun.UseVisualStyleBackColor = true;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(490, 359);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 2;
+            this.btnEnd.Text = "&End Kinect";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(409, 359);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 394);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.chtKinectActivity);
+            this.Name = "Form1";
+            this.Text = "Sleep Detection via Kinect";
+            ((System.ComponentModel.ISupportInitialize)(this.chtKinectActivity)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtKinectActivity;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
